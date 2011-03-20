86a585a20b3b4e0eeac0099d7fd738f2fca1e8a5
Nils Dagsson Moskopp <nils@dieweltistgarnichtso.net>
2011-03-20 06:01:35 +0100

    + eisenhower npc

---
+++ JunkJungle-d5f0a58e52d6/lib/monster/eisenhower.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import random
+import monster
+import sprite
+import util
+
+class Mob(monster.Monster):
+    name = "eisenhower"
+    max_hp = 4
+
+    def act(self, level):
+        player = self.level.player
+        dirs = util.dir_of(self.pos, player.pos)
+        for direction in dirs:
+            try:
+                try:
+                    self.walk(direction)
+                    return
+                except monster.MoveBump, e:
+                    if e.monster == player:
+                        self.attack(direction, e.monster)
+                        return
+            except monster.MoveError:
+                pass
+
+    class Sprite(sprite.Monster):
+        def __init__(self, monster, display):
+            sprite.Monster.__init__(self, monster, display)
+
+        def animate(self, delay=1, frames=None):
+            """Eisenhower has more frames and facing."""
+            sprite.Monster.animate(self, delay, self.frames[self.facing-1])
+
+        def anim_stand(self):
+            """Eisenhower is not animated when standing."""
+            self.image = self.frames[self.facing-1][0]
+            yield None
+
+        def anim_walk(self):
+            dx = util.delta_x[self.facing]*2
+            dy = util.delta_y[self.facing]*2
+            self.move(dx, dy)
+            steps = 7
+            for step in range(steps):
+                self.move(dx, dy)
+                self.animate()
+                yield None
